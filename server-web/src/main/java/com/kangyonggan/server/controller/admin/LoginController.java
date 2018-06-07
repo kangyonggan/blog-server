e19000ebcd35abb12b893ed12f355e75c250e0c2
康永敢 <kangyonggan@gmail.com>
2018-06-07 22:07:10 +0800

    admin api

--- server-web/src/main/java/com/kangyonggan/server/controller/admin/LoginController.java
+++ server-web/src/main/java/com/kangyonggan/server/controller/admin/LoginController.java
@@ -11,13 +11,15 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author kangyonggan
  * @since 5/31/18
  */
-@RestController("api")
+@RestController
+@RequestMapping("api")
 @Log4j2
 public class LoginController extends BaseController {
 
