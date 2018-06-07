ddd4f1d8c0604d368e55dcbfa2b6fe421e1523f7
kangyonggan <kangyonggan@gmail.com>
2018-06-07 18:44:32 +0800

    log

--- server-web/src/main/java/com/kangyonggan/server/interceptor/ParamsInterceptor.java
+++ server-web/src/main/java/com/kangyonggan/server/interceptor/ParamsInterceptor.java
@@ -52,10 +52,12 @@ public class ParamsInterceptor extends HandlerInterceptorAdapter {
                     (request.getHeader("accept") != null && request.getHeader("accept").contains("application/json"))
                     || (request.getHeader("X-Requested-With") != null && request.getHeader("X-Requested-With").contains("XMLHttpRequest")));
             if (isCommonReq) {
+                log.info("重定向到首页");
                 // 乱七八糟的请求全部重定向到首页
                 response.sendRedirect("/");
                 return false;
             } else {
+                log.info("登录失效");
                 // 9998: 登录失效
                 Response resp = Response.getFailureResponse(Resp.INVALID_LOGIN.getRespCo(), Resp.INVALID_LOGIN.getRespMsg());
                 writeResponse(response, resp);
